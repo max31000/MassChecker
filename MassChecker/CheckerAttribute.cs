8ba19021bce8623b709b57c535a8ac305801f3f5
Maxim V <49785937+max31000@users.noreply.github.com>
2024-02-28 23:12:16 +0500

    in-code documentation

--- MassChecker/CheckerAttribute.cs
+++ MassChecker/CheckerAttribute.cs
@@ -1,5 +1,10 @@
 ﻿namespace MassChecker;
 
+/// <summary>
+/// Attribute used to mark methods in classes derived from <see cref="MassCheckerBase{TItem, TFilter}"/>
+/// as conditions for filtering. Only methods marked with this attribute are considered when evaluating
+/// an item against the filter criteria.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method)]
 public class CheckerAttribute : Attribute
 {
--- MassChecker/MassCheckerBase.cs
+++ MassChecker/MassCheckerBase.cs
@@ -3,6 +3,12 @@ using System.Reflection;
 
 namespace MassChecker;
 
+/// <summary>
+/// Serves as the base class for creating filters with multiple conditions.
+/// This generic class allows defining filters based on specified item and filter types.
+/// </summary>
+/// <typeparam name="TItem">The type of the items to be filtered.</typeparam>
+/// <typeparam name="TFilter">The type of the filter to apply to the items.</typeparam>
 public abstract class MassCheckerBase<TItem, TFilter>
 {
     private readonly Func<TItem, TFilter, bool>[] checkers;
@@ -36,8 +42,18 @@ public abstract class MassCheckerBase<TItem, TFilter>
             .ToArray();
     }
 
+    /// <summary>
+    /// An internal array of delegate checkers, each corresponding to a method annotated with the <see cref="CheckerAttribute"/>.
+    /// These delegates are used to evaluate the filter conditions on the items.
+    /// </summary>
     internal IEnumerable<Func<TItem, TFilter, bool>> Checkers => checkers;
 
+    /// <summary>
+    /// Evaluates the item against all filter conditions defined in the derived class.
+    /// </summary>
+    /// <param name="itemDto">The item to evaluate.</param>
+    /// <param name="filterDto">The filter criteria to apply to the item.</param>
+    /// <returns>true if the item matches all filter criteria; otherwise, false.</returns>
     public virtual bool NeedItem(TItem itemDto, TFilter filterDto)
     {
         return checkers.All(predicate => predicate(itemDto, filterDto));
